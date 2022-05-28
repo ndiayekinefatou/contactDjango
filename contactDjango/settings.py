aa8e8f1ae9545ada26db19eb6cd205cf3d62466f
ndiayekinefatou <kinendiaye4991@gmail.com>
2022-05-28 19:06:17 +0000

    update 3 for sqlite3

--- contactDjango/settings.py
+++ contactDjango/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'contactDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'contactDjango',
-        'USER': 'fkine',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': 'fkine',
         #'PASSWORD': 'fkine',
         'PASSWORD': 'fk!ne994',
         'HOST': 'localhost',
